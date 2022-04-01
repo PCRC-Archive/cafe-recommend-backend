d0a3e2d554f326e088e59d3331f28354ab82ee66
Red-Gunny <nehepkldi@naver.com>
2022-04-01 12:55:35 +0900

    [Init] #0 프로젝트 초기 세팅

---
+++ src/main/java/pcrc/caferecommendbackend/CafeRecommendBackendApplication.java
@@ -0,0 +1,13 @@
+package pcrc.caferecommendbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeRecommendBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeRecommendBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pcrc/caferecommendbackend/CafeRecommendBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pcrc.caferecommendbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeRecommendBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
